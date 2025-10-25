85aad6418b20b955be5d1cf6b37fea05dbdfb98b
Alex Arias <aarias077@gmail.com>
2025-10-25 16:38:15 -0500

    wip

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def student_serializer(stud: Students) -> dict:
         'grade': stud.grade
         }
 
-@app.route("/students")
+@app.route("/students", methods=["GET"])
 def students():
     result = []
     all_studs = Students.query.all()
@@ -38,8 +38,63 @@ def students():
         result.append(serialzed)
     return jsonify(result)
 
+@app.route("/old_students", methods=["GET"])
+def old_students():
+    result = []
+    all_students = Students.query.all()
+    for stud in all_students:
+        if stud.age>20:
+            serialized = student_serializer(stud)
+            result.append(serialized)
+    return jsonify(result)
+
+
+
+@app.route("/young_students", methods=["GET"])
+def young_students():
+    result = []
+    all_students = Students.query.all()
+    for stud in all_students:
+        if stud.age>20:
+            serialized = student_serializer(stud)
+            result.append(serialized)
+    return jsonify(result)
+
+
+@app.route("/advance_students", methods=["GET"])
+def advance_students():
+    result = []
+    all_students = Students.query.all()
+    for stud in all_students:
+        if stud.age>20:
+            serialized = student_serializer(stud)
+            result.append(serialized)
+    return jsonify(result)
+
+
+@app.route("/student_names", methods=["GET"])
+def student_names():
+    result = []
+    all_students = Students.query.all()
+    for stud in all_students:
+        if stud.age>20:
+            serialized = student_serializer(stud)
+            result.append(serialized)
+    return jsonify(result)
+
+
+@app.route("/student_ages", methods=["GET"])
+def student_ages():
+    result = []
+    all_students = Students.query.all()
+    for stud in all_students:
+        if stud.age>20:
+            serialized = student_serializer(stud)
+            result.append(serialized)
+    return jsonify(result)
+
 
 
 
 if __name__ == "__main__":
-    app.run(debug= True)
+    app.run(debug= True, port=8000)
